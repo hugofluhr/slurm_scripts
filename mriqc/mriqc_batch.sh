da0ca3b999577534d78c9f06221a911f79859619
hugofluhr <hugo.fluhr@gmail.com>
2024-07-15 11:54:38 +0200

    mriqc script for learning habits

--- mriqc/mriqc_batch.sh
+++ mriqc/mriqc_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mriqc
-#SBATCH -o /home/hfluhr/logs/mriqc/out/%x-%A-%a.out
-#SBATCH -e /home/hfluhr/logs/mriqc/err/%x-%A-%a.err
+#SBATCH -o /home/hfluhr/logs/learning-habits/dev/mriqc/out/%x-%A-%a.out
+#SBATCH -e /home/hfluhr/logs/learning-habits/dev/mriqc/err/%x-%A-%a.err
 ##SBATCH --mail-user=hugo.fluhr@econ.uzh.ch
 ##SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
@@ -12,14 +12,17 @@
 # source /etc/profile.d/lmod.sh
 # Use as:
 # sbatch --array=1-$(( $(wc -l /home/hfluhr/data/gera_data/participants_yifei.tsv | cut -f1 -d' '))) scripts/mriqc/mriqc_batch.sh
+# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/participants.tsv | cut -f1 -d' '))) scripts/mriqc/mriqc_batch.sh
+# sbatch --array=1-3 scripts/mriqc/mriqc_batch.sh
+
 
 module load singularityce
 
-SUBJ_LIST_DIR="/data/hfluhr/gera_data"
-subID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${SUBJ_LIST_DIR}/participants_yifei.tsv)
+SUBJ_LIST_DIR="/data/hfluhr/learninghabits/dev"
+subID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${SUBJ_LIST_DIR}/participants.tsv)
 subject="sub-${subID}"
 echo $subject
 
 #singularity run --cleanenv -B /home/hfluhr/shares-hare/ds004299:/ds004299 -B /scratch/hfluhr/workflows:/workflow /home/hfluhr/data/containers/mriqc /ds004299 /ds004299/derivatives/mriqc participant --participant-label $subject -w /workflow/mriqc_run1 --session-id 3 --run-id 4
 #singularity run --cleanenv /home/hfluhr/data/containers/mriqc --version
-singularity run --cleanenv -B /home/hfluhr/shares-hare/ds004299:/ds004299 -B /scratch/hfluhr/workflows:/workflow /home/hfluhr/data/containers/mriqc /ds004299 /ds004299/derivatives/mriqc participant --participant-label $subject -w /workflow/mriqc_run1 --session-id 3 -m 'T1w'
+singularity run --cleanenv -B /home/hfluhr/shares-hare/ds-learning-habits:/ds-learning-habits -B /scratch/hfluhr/workflows:/workflow /home/hfluhr/data/containers/arch/mriqc /ds-learning-habits /ds-learning-habits/derivatives/mriqc participant --participant-label $subject -w /workflow/learning-habits/dev/mriqc

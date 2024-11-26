98403e96122d7663645d8e16a46b126bc5493530
hugofluhr <hugo.fluhr@gmail.com>
2024-11-26 17:34:07 +0100

    update mriqc script

--- mriqc/mriqc_batch.sh
+++ mriqc/mriqc_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mriqc
-#SBATCH -o /home/hfluhr/logs/learning-habits/dev/mriqc/out/%x-%A-%a.out
-#SBATCH -e /home/hfluhr/logs/learning-habits/dev/mriqc/err/%x-%A-%a.err
+#SBATCH -o /home/hfluhr/logs/learninghabits/mriqc/%x-%A/out/%x-%A-%a.out
+#SBATCH -e /home/hfluhr/logs/learninghabits/mriqc/%x-%A/err/%x-%A-%a.err
 ##SBATCH --mail-user=hugo.fluhr@econ.uzh.ch
 ##SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
@@ -11,7 +11,6 @@
 # what is this line below doing?
 # source /etc/profile.d/lmod.sh
 # Use as:
-# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/gera_data/participants_yifei.tsv | cut -f1 -d' '))) scripts/mriqc/mriqc_batch.sh
 # sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/participants.tsv | cut -f1 -d' '))) scripts/mriqc/mriqc_batch.sh
 # sbatch --array=1-3 scripts/mriqc/mriqc_batch.sh
 
@@ -25,4 +24,4 @@ echo $subject
 
 #singularity run --cleanenv -B /home/hfluhr/shares-hare/ds004299:/ds004299 -B /scratch/hfluhr/workflows:/workflow /home/hfluhr/data/containers/mriqc /ds004299 /ds004299/derivatives/mriqc participant --participant-label $subject -w /workflow/mriqc_run1 --session-id 3 --run-id 4
 #singularity run --cleanenv /home/hfluhr/data/containers/mriqc --version
-singularity run --cleanenv -B /home/hfluhr/shares-hare/ds-learning-habits:/ds-learning-habits -B /scratch/hfluhr/workflows:/workflow /home/hfluhr/data/containers/arch/mriqc /ds-learning-habits /ds-learning-habits/derivatives/mriqc participant --participant-label $subject -w /workflow/learning-habits/dev/mriqc
+singularity run --cleanenv -B /home/hfluhr/shares-hare/ds-learning-habits:/ds-learning-habits -B /scratch/hfluhr/workflows:/workflow /home/hfluhr/data/containers/arch/mriqc /ds-learning-habits /ds-learning-habits/derivatives/mriqc participant --participant-label $subject -w /workflow/learning-habits/dev/mriqc_defaced -m T1w

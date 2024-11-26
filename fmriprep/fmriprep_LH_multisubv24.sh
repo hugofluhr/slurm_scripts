7a13ed84720e0b9f3bf9b0a83f083ef7d380f582
hugofluhr <hugo.fluhr@gmail.com>
2024-11-26 17:33:22 +0100

    batch fmriprep

--- fmriprep/fmriprep_LH_multisubv24.sh
+++ fmriprep/fmriprep_LH_multisubv24.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=fmriprep
-#SBATCH -o /home/hfluhr/logs/learninghabits/fmriprep/%x-%A/out/%x-%A-%a.out
-#SBATCH -e /home/hfluhr/logs/learninghabits/fmriprep/%x-%A/err/%x-%A-%a.err
+#SBATCH -o /home/hfluhr/logs/learninghabits/fmriprep24/%x-%A/out/%x-%A-%a.out
+#SBATCH -e /home/hfluhr/logs/learninghabits/fmriprep24/%x-%A/err/%x-%A-%a.err
 ##SBATCH --mail-user=hugo.fluhr@econ.uzh.ch
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
@@ -10,10 +10,10 @@
 #SBATCH --time=24:00:00
 
 # Use as:
-# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/participants.tsv | cut -f1 -d' '))) scripts/fmriprep/fmriprep_LH_multisub.sh
+# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/participants.tsv | cut -f1 -d' '))) scripts/fmriprep/fmriprep_LH_multisubv24.sh
 
 module load singularityce
-IMG="/home/hfluhr/data/containers/fmriprep23"
+IMG="/home/hfluhr/data/containers/fmriprep24"
 export SINGULARITYENV_FS_LICENSE=$HOME/freesurfer/license.txt
 
 SUBJ_LIST_DIR="/data/hfluhr/learninghabits/dev"
@@ -24,7 +24,7 @@ echo "Processing the anatomical and functional workflows on subject: $SUBJECT"
 # Paths
 DATADIR="/home/hfluhr/shares-hare/ds-learning-habits"
 #DATADIR="/home/hfluhr/data/learninghabits/ds-learninghabits"
-WORKDIR="/scratch/hfluhr/workflows/ds-learninghabits"
+WORKDIR="/scratch/hfluhr/workflows/ds-learninghabits/fmriprep_${SLURM_JOB_ID}"
 mkdir -p ${WORKDIR}
 OUTDIR="${DATADIR}/derivatives"
 mkdir -p $OUTDIR
@@ -34,8 +34,9 @@ BINDINGS="-B $DATADIR:/data \
 -B ${WORKDIR}:/work \
 -B ${OUTDIR}:/out"
 
-FMRIPREP_CMD="/data /out/fmriprep-23.2.1_new participant --participant-label $SUBJECT \
--w /work"
+FMRIPREP_CMD="/data /out/fmriprep-24.0.1-mem participant --participant-label $SUBJECT \
+-w /work \
+--mem-mb 50000"
 
 SING_CMD="singularity run --cleanenv $BINDINGS $IMG $FMRIPREP_CMD"
 echo $SING_CMD
--- fmriprep/fmriprep_LH_multisubv24_debug.sh
+++ fmriprep/fmriprep_LH_multisubv24_debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=fmriprep
-#SBATCH -o /home/hfluhr/logs/learninghabits/fmriprep/%x-%A/out/%x-%A-%a.out
-#SBATCH -e /home/hfluhr/logs/learninghabits/fmriprep/%x-%A/err/%x-%A-%a.err
+#SBATCH -o /home/hfluhr/logs/learninghabits/fmriprep24/%x-%A/out/%x-%A-%a.out
+#SBATCH -e /home/hfluhr/logs/learninghabits/fmriprep24/%x-%A/err/%x-%A-%a.err
 ##SBATCH --mail-user=hugo.fluhr@econ.uzh.ch
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
@@ -10,21 +10,22 @@
 #SBATCH --time=24:00:00
 
 # Use as:
-# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/participants.tsv | cut -f1 -d' '))) scripts/fmriprep/fmriprep_LH_multisub.sh
+# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/participants_remaining.tsv | cut -f1 -d' '))) scripts/fmriprep/fmriprep_LH_multisubv24_debug.sh
+# sbatch --array=1-$(( $(wc -l /home/hfluhr/data/learninghabits/dev/dev_subset.tsv | cut -f1 -d' '))) scripts/fmriprep/fmriprep_LH_multisubv24_debug.sh
 
 module load singularityce
-IMG="/home/hfluhr/data/containers/fmriprep23"
+IMG="/home/hfluhr/data/containers/fmriprep24"
 export SINGULARITYENV_FS_LICENSE=$HOME/freesurfer/license.txt
 
 SUBJ_LIST_DIR="/data/hfluhr/learninghabits/dev"
-subID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${SUBJ_LIST_DIR}/participants.tsv)
+subID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${SUBJ_LIST_DIR}/dev_subset.tsv)
 SUBJECT="sub-${subID}"
 echo "Processing the anatomical and functional workflows on subject: $SUBJECT"
 
 # Paths
-DATADIR="/home/hfluhr/shares-hare/ds-learning-habits"
+DATADIR="/home/hfluhr/data/learninghabits/sdc_test"
 #DATADIR="/home/hfluhr/data/learninghabits/ds-learninghabits"
-WORKDIR="/scratch/hfluhr/workflows/ds-learninghabits"
+WORKDIR="/scratch/hfluhr/workflows/ds-learninghabits/fmriprep_${SLURM_JOB_ID}"
 mkdir -p ${WORKDIR}
 OUTDIR="${DATADIR}/derivatives"
 mkdir -p $OUTDIR
@@ -34,7 +35,7 @@ BINDINGS="-B $DATADIR:/data \
 -B ${WORKDIR}:/work \
 -B ${OUTDIR}:/out"
 
-FMRIPREP_CMD="/data /out/fmriprep-23.2.1_new participant --participant-label $SUBJECT \
+FMRIPREP_CMD="/data /out/fmriprep-24.0.1_sdc_test participant --participant-label $SUBJECT \
 -w /work"
 
 SING_CMD="singularity run --cleanenv $BINDINGS $IMG $FMRIPREP_CMD"
